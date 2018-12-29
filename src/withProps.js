a5cee678b1d0faf4b7724336f59b6c14ddb7d230
Luke Burns <lukemburns@gmail.com>
2018-12-29 12:50:17 -0500

    change overwrite order of props in withProps

--- src/withProps.js
+++ src/withProps.js
@@ -3,7 +3,7 @@ const React = require('react')
 const withProps = (Wrapped, presetProps) => {
   class Wrapper extends Wrapped {
     constructor (props) {
-      super(Object.assign({}, props, presetProps))
+      super(Object.assign({}, presetProps, props))
     }
   }
   return Wrapper
