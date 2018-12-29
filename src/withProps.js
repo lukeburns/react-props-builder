0d12707ddcb4a6b8aeb72a3f38a4bb32d16e918a
Luke Burns <lukemburns@gmail.com>
2018-12-29 12:47:08 -0500

    make withProps more canonical

--- src/withProps.js
+++ src/withProps.js
@@ -1,26 +1,9 @@
 const React = require('react')
 
-const withProps = (Wrapped, preset) => {
-  class Wrapper extends React.Component {
+const withProps = (Wrapped, presetProps) => {
+  class Wrapper extends Wrapped {
     constructor (props) {
-      super(props)
-      this.getState = this.getState.bind(this)
-      this.childRef = React.createRef()
-      this.getState = this.getState.bind(this)
-      this.handleChange = this.handleChange.bind(this)
-    }
-    getState () {
-      return this.childRef.current.getState()
-    }
-    handleChange () {
-      if (this.props.onChange) {
-        this.props.onChange(this.getState())
-      }
-    }
-    render () {
-      return (
-        <Wrapped {...preset} {...this.props} onChange={this.handleChange} ref={this.childRef} />
-      )
+      super(Object.assign({}, props, presetProps))
     }
   }
   return Wrapper
